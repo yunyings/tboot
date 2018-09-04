185fee272aa503e144a934d6e6739ac90eebd0d8
yunyings <yunying.sun@intel.com>
2018-09-04 13:41:12 +0800

    Add files via upload
    Scripts for updating grubx64.efi to use tboot under Fedora/RHEL.

---
+++ create-grub-efi-Fedora.sh
@@ -0,0 +1,7 @@
+GRUB_MODULES="all_video boot btrfs cat chain configfile echo efifwsetup \
+efinet ext2 fat font gfxmenu gfxterm gzio halt hfsplus iso9660 \
+jpeg loadenv lvm mdraid09 mdraid1x minicmd normal part_apple \
+part_msdos part_gpt password_pbkdf2 png reboot search \
+search_fs_uuid search_fs_file search_label sleep syslinuxcfg \
+test tftp regexp video xfs linuxefi multiboot multiboot2"
+grub2-mkimage -O x86_64-efi -o grubx64.efi.new -p /EFI/fedora $GRUB_MODULES
